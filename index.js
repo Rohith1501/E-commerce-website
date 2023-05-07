3c3775f27f2a2a3bbc0948211f3d970c742642d3
Rohith1501 <127459052+Rohith1501@users.noreply.github.com>
2023-05-07 21:47:32 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,37 +1,33 @@
 import React, { Fragment, createContext, useReducer } from "react";
 import Layout from "../layout";
-import Slider from "./Slider";
-import ProductCategory from "./ProductCategory";
-import { homeState, homeReducer } from "./HomeContext";
-import SingleProduct from "./SingleProduct";
+import {
+  productDetailsState,
+  productDetailsReducer,
+} from "./ProductDetailsContext";
+import Details from "./Details";
 
-export const HomeContext = createContext();
+export const ProductDetailsContext = createContext();
 
-const HomeComponent = () => {
+const DetailsComponent = () => {
   return (
     <Fragment>
-      <Slider />
-      {/* Category, Search & Filter Section */}
-      <section className="m-4 md:mx-8 md:my-6">
-        <ProductCategory />
-      </section>
-      {/* Product Section */}
-      <section className="m-4 md:mx-8 md:my-6 grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
-        <SingleProduct />
-      </section>
+      <Details />
     </Fragment>
   );
 };
 
-const Home = (props) => {
-  const [data, dispatch] = useReducer(homeReducer, homeState);
+const ProductDetails = (props) => {
+  const [data, dispatch] = useReducer(
+    productDetailsReducer,
+    productDetailsState
+  );
   return (
     <Fragment>
-      <HomeContext.Provider value={{ data, dispatch }}>
-        <Layout children={<HomeComponent />} />
-      </HomeContext.Provider>
+      <ProductDetailsContext.Provider value={{ data, dispatch }}>
+        <Layout children={<DetailsComponent />} />
+      </ProductDetailsContext.Provider>
     </Fragment>
   );
 };
 
-export default Home;
+export default ProductDetails;
